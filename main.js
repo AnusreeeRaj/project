062be09b39ae087c7bbe3da2a595eefa26652cf9
AnusreeeRaj <rajanusree375@gmail.com>
2024-12-27 12:18:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// let, const
+
+let age= 30;
+console.log(age);
+
+const score=10;
+console.log(score);
+
+// string,numbers,boolean,null,undefined
+
+const name = 'John';
+const ageN = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+
+// concatination
+console.log('my name is '+name+ ' and i am '+ageN);
+//template string
+const hello =`my name is ${name} and i am ${ageN}`;
+
+console.log(hello);
+const s = 'hello world';
+console.log(s.split(''));
