dbdac4963b1f1b213cb229b51b5dffe0759cd914
AnusreeeRaj <rajanusree375@gmail.com>
2024-12-29 16:00:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -28,3 +28,48 @@ const hello =`my name is ${name} and i am ${ageN}`;
 console.log(hello);
 const s = 'hello world';
 console.log(s.split(''));
+
+// arrays
+const fruits =['oranges','apple','cherry'];
+fruits[3]='kiwi';
+fruits.push('mango');
+fruits.unshift('grapes');
+console.log(Array.isArray('hello'));
+console.log(fruits.indexOf('grapes'));
+console.log(fruits);
+
+
+const person = {
+    firstName: 'John',
+    lastName: 'Wick',
+    age: 30,
+    hobbies: ['fighting','movies'],
+    address:{
+        street: 'blah',
+        city: 'blah blah',
+        state: 'blah blah blah'
+    }
+}
+
+console.log(person);
+console.log(person.firstName);
+console.log(person.hobbies[1]);
+
+const todos =[
+    {
+        id:1,
+        text:'take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'cooking',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'appointments',
+        isCompleted: true
+    }
+];
+console.log(todos);
